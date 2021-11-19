c9d82238b052900504cd42697fd5e0ed8421d390
inflationtea <gravityfries@gmail.com>
2021-11-19 10:00:17 +0800

    backup

---
+++ app/src/main/java/com/example/prelim_exam/HomeFragment.kt
@@ -0,0 +1,46 @@
+package com.example.prelim_exam
+
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.example.prelim_exam.databinding.FragmentHomeBinding
+
+
+class HomeFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val binding = DataBindingUtil.inflate<FragmentHomeBinding>(inflater,
+        R.layout.fragment_home, container, false)
+
+        binding.btnAbout.setOnClickListener {
+            view: View -> view.findNavController().navigate(R.id.action_homeFragment_to_aboutFragment)
+        }
+        binding.btnContent.setOnClickListener {
+                view: View -> view.findNavController().navigate(R.id.action_homeFragment_to_contentFragment)
+        }
+        binding.btnFaq.setOnClickListener {
+                view: View -> view.findNavController().navigate(R.id.action_homeFragment_to_faqFragment)
+        }
+        setHasOptionsMenu(true)
+        return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.options_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item,requireView().findNavController())
+                || super.onOptionsItemSelected(item)
+    }
+
+}
---
+++ app/src/main/java/com/example/prelim_exam/LoginFragment.kt
@@ -0,0 +1,44 @@
+package com.example.prelim_exam
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import com.example.prelim_exam.databinding.FragmentLoginBinding
+
+
+class LoginFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val binding = DataBindingUtil.inflate<FragmentLoginBinding>(inflater,
+        R.layout.fragment_login, container, false)
+
+        val navController = findNavController()
+        binding.btnRegister.setOnClickListener { view: View ->
+            view.findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
+        }
+        binding.btnLogin.setOnClickListener {
+            val username = binding.editUsername.text.toString()
+            val pass = binding.editPassword.text.toString()
+
+            if(username == "Beatrix" && pass == "hello123"){
+                navController.navigate(R.id.action_loginFragment_to_homeFragment)
+            }else {
+                Toast.makeText(requireActivity(),"Incorrect username or password!", Toast.LENGTH_LONG).show()
+            }
+        }
+        return binding.root
+
+    }
+
+}
